5602985324d0ef49459e6b9716a6971324d747ee
Lau1910 <1.garcia.aguilar@gmail.com>
2024-09-08 12:56:03 +0200

    Colocacion y uso boton volver
    Añado un script de controlador Volver para asignarlo a los botones de los ejercicios para volver a la pantalla de ejercicios

---
+++ AlzAware/Assets/Scripts/Controladores/ControladorVolver.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorVolver : MonoBehaviour
+{
+    // Método utilizado para volver a Ejercicios
+    public void VolverAEjercicios()
+    {
+        SceneManager.LoadScene("Ejercicios");
+    }
+}
